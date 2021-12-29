5566226c1a9c4f7d7ad7a5633d34d9fda463c7a6
Shailendra Sirohi <shailendra.sirohi@iiml.org>
2021-12-29 13:11:31 +0530

    cp: IsUpToDate completed

--- internal/clients/sns/topicclient.go
+++ internal/clients/sns/topicclient.go
@@ -9,6 +9,8 @@ import (
 	"github.com/aws/smithy-go"
 	"provider-aws-controlapi/apis/sns/v1alpha1"
 	awsclient "provider-aws-controlapi/internal/clients"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -45,7 +47,7 @@ func LateInitialize(in *v1alpha1.TopicParameters,attributes map[string]string, t
 	if in.Tags == nil && len(tags) > 0 {
 		in.Tags = map[string]string{}
 		for _, v := range tags {
-			in.Tags[*v.Key] = *v.Value
+			in.Tags[aws.ToString(v.Key)] = aws.ToString(v.Value)
 		}
 	}
 
@@ -71,5 +73,46 @@ func GenerateObservation(attributes map[string]string) v1alpha1.TopicObservation
 		EffectiveDeliveryPolicy: aws.String(attributes[v1alpha1.TopicEffectiveDeliveryPolicy]),
 	}
 	return ob
+}
+
+// IsUpToDate returns true if the Topic attributes in AWS
+// are same as Topic spec, else returns false
+func IsUpToDate(p v1alpha1.TopicParameters, attributes map[string]string, tags []types.Tag) bool{
+
+	if len(p.Tags) != len(tags){
+		return false
+	}
+
+	for _,v := range tags{
+		tagVal, ok := p.Tags[aws.ToString(v.Key)]
+		if !ok || !strings.EqualFold(tagVal,aws.ToString(v.Value)){
+			return false
+		}
+	}
+
+	if !strings.EqualFold(aws.ToString(p.Policy),attributes[v1alpha1.TopicPolicy]){
+		return false
+	}
+	if !strings.EqualFold(aws.ToString(p.DisplayName),attributes[v1alpha1.TopicDisplayName]){
+		return false
+	}
+	if !strings.EqualFold(aws.ToString(p.DeliveryPolicy),attributes[v1alpha1.TopicDeliveryPolicy]){
+		return false
+	}
+
+	if !strings.EqualFold(aws.ToString(p.KMSMasterKeyID),attributes[v1alpha1.TopicKMSMasterKeyID]){
+		return false
+	}
+
+	b, e := strconv.ParseBool(attributes[v1alpha1.FifoTopic])
+	if e != nil || aws.ToBool(p.FifoTopic) != b{
+		return false
+	}
+
+	b, e = strconv.ParseBool(attributes[v1alpha1.FifoTopicContentBasedDeduplication])
+	if e != nil || aws.ToBool(p.ContentBasedDeduplication) != b{
+		return false
+	}
+	return true
 }
 
